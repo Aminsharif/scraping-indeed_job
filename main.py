0d4e3ea9ec701d59f0dd2c7e8bef6424571d32e2
Amin Sharif <33375704+Aminsharif@users.noreply.github.com>
2024-12-31 16:10:11 +0600

    completed

---
+++ main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from utils import *
+
+load_dotenv()
+
+"""
+List of countries url.
+"""
+nigeria = 'https://ng.indeed.com'
+united_kingdom = 'https://uk.indeed.com'
+united_states = 'https://www.indeed.com'
+canada = 'https://ca.indeed.com'
+germany = 'https://de.indeed.com'
+australia = 'https://au.indeed.com'
+south_africa = 'https://za.indeed.com'
+sweden = 'https://se.indeed.com'
+singapore = 'https://www.indeed.com.sg'
+switzerland = 'https://www.indeed.ch'
+united_arab_emirates = 'https://www.indeed.ae'
+new_zealand = 'https://nz.indeed.com'
+india = 'https://www.indeed.co.in'
+france = 'https://www.indeed.fr'
+italy = 'https://it.indeed.com'
+spain = 'https://www.indeed.es'
+japan = 'https://jp.indeed.com'
+south_korea = 'https://kr.indeed.com'
+brazil = 'https://www.indeed.com.br'
+mexico = 'https://www.indeed.com.mx'
+china = 'https://cn.indeed.com'
+saudi_arabia = 'https://sa.indeed.com'
+egypt = 'https://eg.indeed.com'
+thailand = 'https://th.indeed.com'
+vietnam = 'https://vn.indeed.com'
+argentina = 'https://ar.indeed.com'
+ireland = 'https://ie.indeed.com'
+
+def main():
+    driver = configure_webdriver()
+    country = india
+    job_position = 'javascript'
+    job_location = 'remote'
+    date_posted = 20
+
+    try:
+        search_jobs(driver, country, job_position, job_location, date_posted)
+        df = scrape_job_data(driver, country) 
+        save_csv(df,job_position, job_location)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        driver.quit()   
+
+
+
+if __name__ == "__main__":
+    main()
